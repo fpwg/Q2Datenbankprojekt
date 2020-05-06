08b7746312dcb56244c145e015ab0ff144e30eb5
fpwg <46819544+fpwg@users.noreply.github.com>
2020-05-06 08:32:31 +0200

    Usermodel um Passwortfunktionen ergänzt

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Email, Length, EqualTo
+from wtforms.validators import DataRequired, Email, Length, EqualTo, ValidationError
 from app.models import User
 
 class LoginForm(FlaskForm):
