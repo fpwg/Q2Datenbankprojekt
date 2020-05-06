aa4e1535fdc187106a4c4f7a705e4bb2e091b7af
fpwg <46819544+fpwg@users.noreply.github.com>
2020-05-06 08:33:07 +0200

    Datenbankregistrierung hinzugefügt

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from app import app
+from flask import render_template, redirect, url_for
+from app import app, db
 from app.forms import LoginForm, RegistrationForm
+from app.models import User
 
 @app.route('/')
 @app.route('/index')
@@ -12,7 +13,14 @@ def login():
     form = LoginForm()
     return render_template('login.html', title='Sign In', form=form)
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = LoginForm()
-    return render_template('login.html', title='Register', form=RegistrationForm)
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print("val")
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=RegistrationForm())
