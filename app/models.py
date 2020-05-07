8f6478a7adedf27843ad5a06db66a3642df1f98b
Hrinutss <64860635+Hrinutss@users.noreply.github.com>
2020-05-07 14:36:13 +0200

    Organisation und User/Organisation Beziehung hinzugefügt

--- app/models.py
+++ app/models.py
@@ -22,3 +22,11 @@ class User(UserMixin, db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
+class Organisation(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), index = True, unique = True)
+
+organisation_user = db.Table('organisation_user',
+db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+db.Column('organisation_id', db.Integer, db.ForeignKey('organisation.id'),primary_key=True)
+)
