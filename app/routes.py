ad21daa204a46e88cc773589590cad460887d9b5
Hrinutss <64860635+Hrinutss@users.noreply.github.com>
2020-05-11 12:13:06 +0200

    Alle Forms funktionsfähig

--- app/routes.py
+++ app/routes.py
@@ -98,9 +98,6 @@ def organisation(name):
     organisation = Organisation.query.filter_by(name=name).first_or_404()
 
     join_form = JoinForm()
-    if join_form.validate_on_submit():
-        organisation.add_user(current_user)
-        db.session.commit()
 
     all_user = User.query.all()
     for u in all_user:
@@ -109,15 +106,37 @@ def organisation(name):
     add_user_form = UserOrganisationForm(obj=all_user)
     add_user_form.user.choices = [(u.id, u.username) for u in all_user]
     add_user_form.user.choices.insert(0, (-1,"choose"))
-    if add_user_form.validate_on_submit() and not add_user_form.user.data == -1:
-        new_user_id = add_user_form.user.data
-        print(new_user_id)
-        new_user = User.query.get(new_user_id)
-        print (new_user)
-        organisation.add_user(new_user)
 
     user_in_organisation = organisation.user
     remove_user_form = UserOrganisationForm(obj=user_in_organisation)
+    remove_user_form.user.choices = [(u.id, u.username) for u in user_in_organisation]
+    remove_user_form.user.choices.insert(0, (-1, "choose"))
+
+    if join_form.join.data and join_form.validate():
+        organisation.add_user(current_user)
+        db.session.commit()
+
+    if add_user_form.submit.data and add_user_form.validate() and not add_user_form.user.data == -1:
+        new_user_id = add_user_form.user.data
+        new_user = User.query.get(new_user_id)
+        organisation.add_user(new_user)
+        db.session.commit()
+
+    if remove_user_form.submit.data and remove_user_form.validate() and not remove_user_form.user.data == -1:
+        old_user_id = remove_user_form.user.data
+        old_user = User.query.get(old_user_id)
+        organisation.remove_user(old_user)
+        db.session.commit()
+
+    all_user = User.query.all()
+    for u in all_user:
+        if organisation in u.organisations:
+            all_user.remove(u)
+
+    user_in_organisation = organisation.user
+
+    add_user_form.user.choices = [(u.id, u.username) for u in all_user]
+    add_user_form.user.choices.insert(0, (-1,"choose"))
     remove_user_form.user.choices = [(u.id, u.username) for u in user_in_organisation]
     remove_user_form.user.choices.insert(0, (-1, "choose"))
 
