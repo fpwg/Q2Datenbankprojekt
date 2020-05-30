5b5db039bdfe496951756b3d98445b90699842eb
Hrinutss <64860635+Hrinutss@users.noreply.github.com>
2020-05-30 22:40:50 +0200

    get index von den verschiedenen spalten

--- app/read_csv.py
+++ app/read_csv.py
@@ -7,20 +7,21 @@ def read_the_file(document):
         read = csv.reader(file, delimiter=';')
         data = []
         for row in read:
-            # Auswahl der richtigen Datenreihen
-            if row[0] and row[1] and not row[0] == "Artikel":
-                # Umgehen von so ner Excel Sache in unserer Inventurliste
+            # Auswahl von Reihen mit Inhalt
+            if row[0] and row[1]:
+                # Umgehen von so ner Excel Sache in (unserer) Inventurliste
                 if row[5] == '0':
                     row[5] = '/'
-                # Da es "Überkategorien" gibt: Diese noch der Kategorie"spalte" hinzufügen
-                x = row[4]
-                row[4] = []
-                row[4].append(kat)
-                row[4].append(x)
+                # Wenn es "Überkategorien" gibt: Diese noch der Kategorie"spalte" hinzufügen
+                if kat and data:
+                    x = row[4]
+                    row[4] = []
+                    row[4].append(kat)
+                    row[4].append(x)
                 # Zeile der Datenliste hinzufügen
                 data.append(row)
             elif row[0]:
-                # Wenn es keine richtige Datenzeile ist, bedeutet das, dass hier eine Oberkategorie steht - wird hiermit eingelesen
+                # Wenn es keine richtige Datenzeile ist, bedeutet das, dass hier eine Oberkategorie steht - wird hiermit erkannt
                 kat = row[0]
     return data
 
@@ -48,3 +49,28 @@ def get_status(ind, data):
         if not i[ind] in status:
             status.append(i[ind])
     return status
+
+"""Herausfinden, wo was steht"""
+def get_indexes(data):
+    article = -1
+    room = -1
+    status = -1
+    category = -1
+    count = -1
+    description = -1
+    row = [x.casefold() for x in data[0]]
+    if "artikel".casefold() in row or "article".casefold() in row:
+        for i in row:
+            if i == "artikel".casefold() or i == "article".casefold():
+                article = row.index(i)
+            if i == "room".casefold() or i == "raum".casefold() or i == "ort".casefold():
+                room = row.index(i)
+            if i == "status".casefold() or i == "zustand".casefold() or i == "funktion".casefold():
+                status = row.index(i)
+            if i == "category".casefold() or i == "kategorie".casefold():
+                category = row.index(i)
+            if i == "anzahl".casefold() or i == "count".casefold():
+                count = row.index(i)
+            if i == "bemerkung".casefold() or i == "beschreibung".casefold or i == "description".casefold():
+                description = row.index(i)
+        return article, room, status, category, count
